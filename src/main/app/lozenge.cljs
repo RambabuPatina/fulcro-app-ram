3df82aa88d06384b0afaddfb9bfd8a7ff355a315
Rambabu Patina <rambabu.patina@appfire.com>
2023-05-03 16:33:16 +0530

    Latest changes

---
+++ src/main/app/lozenge.cljs
@@ -0,0 +1,6 @@
+(ns app.lozenge
+  (:require
+   ["@atlaskit/lozenge" :default Lozenge]
+   [com.fulcrologic.fulcro.algorithms.react-interop :as react-interop]))
+
+(def ui-lozenge (react-interop/react-factory Lozenge))
